1955c31c96556798b46bb65b8bdde839883012e2
Yash Baviskar <104298817+YashBaviskar1@users.noreply.github.com>
2025-03-20 23:12:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import {
   Routes
 } from "react-router-dom";
 import Sports from './Sports/Sports';
-import Cultural from './Cultural/Cultural';
-import Events from './Cultural/Events';
+
 import Login from './Sports/Login';
 import Schedule from "./Sports/Schedule"
 import Council from './Council/Council';
@@ -21,7 +20,7 @@ function App() {
     <Routes>
       <Route path="/" exact element={<Index/>} />
       <Route path="/sports" exact element={<Sports />} />
-      <Route path="/cultural" exact element={<Cultural />} />
+
       <Route path="/events" exact element={<Events />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/schedule" exact element={<Schedule />} />
